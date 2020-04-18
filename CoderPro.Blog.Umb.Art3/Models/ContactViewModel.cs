8536996c9df88d2afef22eb0b29a8a2a10f6ecd9
Brandon Osborne <brandon.osborne@coderpro.net>
2020-04-18 07:42:37 -0500

    Added 'Umbraco 8 - User Handling & Security.'

---
+++ CoderPro.Blog.Umb.Art3/Models/ContactViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace USD.Umbraco.Article3.UI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ContactViewModel
+    {
+        [MinLength(2), MaxLength(25)]
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [MinLength(2), MaxLength(50)]
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [MinLength(5), MaxLength(100)]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string FromEmailAddress { get; set; }
+
+        [MinLength(5), MaxLength(100)]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string ToEmailAddress { get; set; }
+
+        [MinLength(8), MaxLength(2000)]
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
