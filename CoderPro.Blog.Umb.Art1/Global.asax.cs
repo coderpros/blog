9bd5a0b2b446f7f146080b7cef1217c6f9cca069
Brandon Osborne <brandon.osborne@coderpro.net>
2020-04-18 05:59:55 -0500

    Added code for first article

---
+++ CoderPro.Blog.Umb.Art1/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿namespace USD.Umbraco.Article1.UI
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
