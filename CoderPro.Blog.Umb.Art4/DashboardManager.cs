5b48669952fffbf7ef3ea9ce6cb496d052de95ea
Brandon Osborne <brandon.osborne@coderpro.net>
2020-04-20 14:07:21 -0500

    Fixed naming issue with the project for the second article.

---
+++ CoderPro.Blog.Umb.Art4/DashboardManager.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DashboardManager.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the DashboardManager type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace USD.Umbraco.Article3.UI
+{
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Composing;
+    using global::Umbraco.Web;
+    using global::Umbraco.Web.Dashboards;
+
+    /// <summary>
+    /// The dashboard manager.
+    /// </summary>
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    public class DashboardManager : IUserComposer
+    {
+        /// <summary>
+        /// The compose.
+        /// </summary>
+        /// <param name="composition">
+        /// The composition.
+        /// </param>
+        public void Compose(Composition composition)
+        {
+            composition.Dashboards().Remove<FormsDashboard>();
+        }
+    }
+}
